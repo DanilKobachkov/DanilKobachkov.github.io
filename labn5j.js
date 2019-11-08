9d5d46c4d24d579a3f692a5e36350f3184642613
Randomunzo <54925859+Randomunzo@users.noreply.github.com>
2019-11-08 20:28:22 +0700

    Update labn5j.js

--- labn5j.js
+++ labn5j.js
@@ -1,11 +1,71 @@
-calc.onclick = function(){
-    var myform = this.form,
-        chbx = myform['precipitation[]'],
-        chbxSum = 0;
-    for(var i = 0; i < chbx.length; i++){
-        if(chbx[i].checked) {
-            chbxSum += +chbx[i].value;
+function test(quest, value) {
+    if (quest == 4 || quest == 5 || quest == 6){
+        if (array[quest] == undefined){
+            array[quest] = String(value);
+            console.log(value);
+        }
+        else{
+            array[quest] += String(value);
+            console.log(value);
         }
     }
-    myform.result.value = +myform.myname.value + +myform.street.value + chbxSum;
+    else{
+        array[quest] = String(value);
+        console.log(value);
+    }
+};
+
+function checkArray(){
+    for (let i = 0; i < array.length; i++){
+        console.log((i+1) + ") " + array[i]);
+    }
+};
+
+function checkTrue(){
+    let t = 0;
+    for (let i = 0; i < array.length; i++) {
+        if ((i == 4 || i == 5 || i == 6) && (array[i] != undefined)){
+            let arrayMore = array[i].split('');
+            let arrayMoreTrue = trueArray[i].split('');
+            let x = 0;
+            for (let i = 0; i < arrayMore.length; i++) {
+                for (let j = 0; j < arrayMoreTrue.length; j++) {
+                    if (arrayMore[i] == arrayMoreTrue[j]){
+                        x++;
+                        break;
+                    }
+                }
+            }
+            if (x == trueArray[i].length) {
+                t++;
+            }
+        }
+        else {
+            if (array[i] == trueArray[i]){
+                t++;
+            }
+        }
+
+    }
+    document.getElementById('place-for-result').innerHTML='Количество правильных ответов: ' + t;
+    document.getElementById('PDF').style.display = 'block';
+
+    let inputs = document.getElementsByClassName('test-dis');
+    for (let i = 0; i < inputs.length; i++){
+        inputs[i].disabled = true;
+    }
+};
+
+function resetAnswer(quest) {
+    array[quest] = undefined;
+};
+
+function enable(){
+    let inputs = document.getElementsByClassName('test-dis');
+    for (let i = 0; i < inputs.length; i++){
+        inputs[i].disabled = false;
+    }
 }
+
+let array = new Array(10);
+let trueArray = ['Nirvana', '3', '2', '2', '1234', '24', '4', 'a', 'html', 'ol'];
