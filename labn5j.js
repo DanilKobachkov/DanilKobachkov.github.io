c96aca7ffd64168bf4b11ee24efc65de1acb1ff8
Randomunzo <54925859+Randomunzo@users.noreply.github.com>
2019-11-08 20:45:10 +0700

    Update labn5j.js

--- labn5j.js
+++ labn5j.js
@@ -1,71 +1,34 @@
-function test(quest, value) {
-    if (quest == 4 || quest == 5 || quest == 6){
-        if (array[quest] == undefined){
-            array[quest] = String(value);
-            console.log(value);
+ <script>
+        let array_of_correct_answers = ['Coleoptera', 'Жук-титан', 'Cockroach', 'The Beetles', 'Hawker', 'Lumberjack titanium', 'Carnivorous', 'Omnivores', 'Numerous', 'Bronze', 'Плавунец'];
+        let numner_questions = 10;
+        let a = [];
+        function proverka(j = 0) {
+            let elements = document.getElementsByName(j);
+            for (let i = 0, len = elements.length; i < len; ++i) {
+                if(elements[i].type=="text")
+                    a.push(elements[i].value);
+                if (elements[i].checked)
+                    a.push(elements[i].value);
+            }
+            j++;
+            if (j < numner_questions)
+                proverka(j);
+            return a;
         }
-        else{
-            array[quest] += String(value);
-            console.log(value);
-        }
-    }
-    else{
-        array[quest] = String(value);
-        console.log(value);
-    }
-};
-
-function checkArray(){
-    for (let i = 0; i < array.length; i++){
-        console.log((i+1) + ") " + array[i]);
-    }
-};
-
-function checkTrue(){
-    let t = 0;
-    for (let i = 0; i < array.length; i++) {
-        if ((i == 4 || i == 5 || i == 6) && (array[i] != undefined)){
-            let arrayMore = array[i].split('');
-            let arrayMoreTrue = trueArray[i].split('');
-            let x = 0;
-            for (let i = 0; i < arrayMore.length; i++) {
-                for (let j = 0; j < arrayMoreTrue.length; j++) {
-                    if (arrayMore[i] == arrayMoreTrue[j]){
-                        x++;
-                        break;
-                    }
+        function cout() {
+            let n = 0;
+            let p = proverka();
+             if (p.length < numner_questions) {
+              alert("Ответьте на все вопросы")
+             }
+             else {
+            for (let i = 0; i < numner_questions; i++) {
+                if (p[i] == array_of_correct_answers[i]) {
+                    n++;
                 }
             }
-            if (x == trueArray[i].length) {
-                t++;
-            }
+            document.getElementById('id01').style.display = 'block'
+            document.getElementById("sss").innerHTML = "Првильных ответов: " + n;
+             }
         }
-        else {
-            if (array[i] == trueArray[i]){
-                t++;
-            }
-        }
-
-    }
-    document.getElementById('place-for-result').innerHTML='Количество правильных ответов: ' + t;
-    document.getElementById('PDF').style.display = 'block';
-
-    let inputs = document.getElementsByClassName('test-dis');
-    for (let i = 0; i < inputs.length; i++){
-        inputs[i].disabled = true;
-    }
-};
-
-function resetAnswer(quest) {
-    array[quest] = undefined;
-};
-
-function enable(){
-    let inputs = document.getElementsByClassName('test-dis');
-    for (let i = 0; i < inputs.length; i++){
-        inputs[i].disabled = false;
-    }
-}
-
-let array = new Array(10);
-let trueArray = ['Nirvana', '3', '2', '2', '1234', '24', '4', 'a', 'html', 'ol'];
+    </script>
