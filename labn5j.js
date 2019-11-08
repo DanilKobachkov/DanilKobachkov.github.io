543f8bc31c484a81f7c48d5d95efd7a0902185c6
Randomunzo <54925859+Randomunzo@users.noreply.github.com>
2019-11-08 20:48:42 +0700

    Update labn5j.js

--- labn5j.js
+++ labn5j.js
@@ -1,5 +1,5 @@
  <script>
-        let array_of_correct_answers = ['Coleoptera', 'Жук-титан', 'Cockroach', 'The Beetles', 'Hawker', 'Lumberjack titanium', 'Carnivorous', 'Omnivores', 'Numerous', 'Bronze', 'Плавунец'];
+        let array_of_correct_answers = ['Nirvana', 'Led Zeppelin', 'Queen', 'The Beetles', 'Gordon Sumner"', 'The four horsemen', 'Unforgiven 2', 'Axl Rose', 'Foo Fighters', 'Bronze', 'Плавунец'];
         let numner_questions = 10;
         let a = [];
         function proverka(j = 0) {
