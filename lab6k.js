2fe6f99f8a6c903c44ac77e35869da49ca10d6cc
Randomunzo <54925859+Randomunzo@users.noreply.github.com>
2019-11-27 19:21:12 +0700

    Update lab6k.js

--- lab6k.js
+++ lab6k.js
@@ -1,3 +1,8 @@
-for (var i=0; i<9; i++){
+window.onload = function(){
+  for (var i=0; i<9; i++){
   document.getElementById('game').innerHTML+='<div class="block"></div>'; 
+}
+  document.getElementById('game').onclick= function(event){
+    console.log(event);
+  }
 }
